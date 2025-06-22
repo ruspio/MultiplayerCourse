bb720ca715f0ff475b422584f073a3402ff94bb5
ruspio <piotr.k.russek@gmail.com>
2025-06-22 21:38:06 +0200

    Learning about HasAuthority(), Variable Replications and OnRep calls

---
+++ Source/MultiplayerCourse/MyBox.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyBox.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values
+AMyBox::AMyBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	ReplicatedVar = 100.f;
+	//bReplicates = true;
+}
+
+// Called when the game starts or when spawned
+void AMyBox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetReplicates(true);
+	SetReplicateMovement(true);
+}
+
+// Called every frame
+void AMyBox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+#if 0
+	if (HasAuthority())
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, TEXT("Server"), true);
+	} else {
+		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, TEXT("Server"), true);
+	}
+#endif
+}
+
+void AMyBox::OnRep_ReplicatedVar()
+{
+	if (HasAuthority())
+	{
+		FVector NewLocation = GetActorLocation() + FVector(0.f, 0.f, 200.f);
+		SetActorLocation(NewLocation);
+		
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Green, TEXT("Server: OnRep_ReplicatedVar"), true);
+	} else {
+		int32 EditorID = UE::GetPlayInEditorID();
+		GEngine->AddOnScreenDebugMessage(-1, 2.f, FColor::Yellow, 
+			FString::Printf(TEXT("Client %d: OnRep_ReplicatedVar"), EditorID), true
+		);
+	}
+	
+}
+
+void AMyBox::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const 
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AMyBox, ReplicatedVar);
+}
+
---
+++ Source/MultiplayerCourse/MyBox.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyBox.generated.h"
+
+UCLASS()
+class MULTIPLAYERCOURSE_API AMyBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	//
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(ReplicatedUsing = OnRep_ReplicatedVar, BlueprintReadWrite)
+	float ReplicatedVar;
+
+	UFUNCTION(BlueprintCallable)
+	void OnRep_ReplicatedVar();
+
+	void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+};
